6b4ed2618147aaf6f7a9cd69bebaace4408cc973
Sean Byrne <spickles@Seans-iMac.local>
2017-12-15 20:02:01 -0500

    HTML shell is set up, created header.css, list.css, and form.css, and all css files and app.js and angular are linked to index.html

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+const app = angular.module('traveler_tracker_App', []);
+
+
+app.controller('MainController', ['$http', function($http) {
+  // console.log('Hey');
+  // this.test = 'What!';
+
+  // Routes
+
+  
+
+}]);
